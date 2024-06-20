617082ec38ed3519ff462e329d9d780951ec8fa9
shivhub09 <shivnagori2020@gmail.com>
2024-06-20 12:42:33 +0530

    server side setup for dot env

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const app = require("./app");
+require('dotenv').config("./");
 
-app.listen(3000 , ()=>{
-    console.log("App has started to listen on port 3000");
-});
+(async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+
+        console.log("MongoDB connection established");
+        // console.log(require('dotenv').config())
+        app.listen(process.env.PORT, () => {
+            console.log("Express app is connected to SAND ONE & listening on port 8080");
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+
+        process.exit(1);
+    }
+})();
 
