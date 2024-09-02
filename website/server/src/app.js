ae742f4afd1d2e07a7ad136fc5e8b9f7dc8d585c
abhimanyu <abhimanyugaur16@gmail.com>
2024-09-02 16:49:01 +0530

    inside cors added some code

--- website/server/src/app.js
+++ website/server/src/app.js
@@ -2,7 +2,12 @@ const express = require("express")
 const app = express();
 var cors = require('cors');
 const cookieParser = require("cookie-parser")
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://sand-one-website-client.vercel.app',
+        credentials: true
+    }
+));
 
 app.use(express.json({limit: "20mb"}))
 app.use(express.urlencoded({extended:true, limit:"20mb"}))
