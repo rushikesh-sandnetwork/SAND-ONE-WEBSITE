83801f90152731d8d8c8dfb04880794c9eb26c82
abhimanyu <abhimanyugaur16@gmail.com>
2024-10-08 12:08:48 +0530

    punch out logic changed

--- website/server/src/controllers/promoter.controller.js
+++ website/server/src/controllers/promoter.controller.js
@@ -270,7 +270,7 @@ const fillAttendancePunchIn = asyncHandler(async (req, res) => {
             promoterId: promoterId,
             date: currentDate,
             punchInTime: punchInTime,
-            punchOutTime:null,
+            punchOutTime:punchInTime,
             punchInImage: logInFinalImage.url,
             punchOutImage: '',
             
